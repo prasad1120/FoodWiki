7a64ab49f226b36bdb216faf1f6b847107b0cfe6
Prasad <prasadshinde1120@gmail.com>
2022-01-07 00:22:06 -0600

    Create basic table view controller

--- FoodWiki/Controller/MealsTableVC.swift
+++ FoodWiki/Controller/MealsTableVC.swift
@@ -13,7 +13,33 @@ class MealsTableVC: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
 
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! CategoryCell
+        
+        cell.someLabel.text = String(indexPath.row)
+        return cell
+    }
 }
 
+
+//extension MealsTableVC: UITableViewDataSourcePrefetching {
+//    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+//        <#code#>
+//    }
+//
+//    func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+//        <#code#>
+//    }
+//}
+//
---
+++ FoodWiki/View/CategoryCell.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryCell.swift
+//  FoodWiki
+//
+//  Created by Prasad Shinde on 1/6/22.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var someLabel: UILabel!
+    
+}
